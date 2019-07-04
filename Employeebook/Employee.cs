f85a5f5bfc26178405f00465473f334164ee4a2d
lavi09 <42918272+lavi09@users.noreply.github.com>
2019-07-04 12:37:42 -0700

    uwp

---
+++ Employeebook/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeebook
+
+{
+    
+    class Employee
+    {
+        private const string TEXT_FILENAME = "EmployeefileText.txt";
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        // Creating a Dummy Employee method to understand binding concept
+        public static Employee GetEmployee()
+
+        {
+            var employee = new Employee
+            {
+                Name = "John doe",
+                Title = "CEO"
+            };
+            return employee;
+        }
+
+        public static void WriteEmployee(Employee employee)
+        {
+            var employeeData = $"{employee.Name},{employee.Title}";
+            FileHelper.WriteTextfileAsync(TEXT_FILENAME,employeeData);
+
+        }
+        public async static Task <ICollection<Employee>>GetEmployeesAsync()
+        {
+            var employees = new List<Employee>();
+            var fileContent= await FileHelper.ReadTextFileAsync(TEXT_FILENAME);
+            var lines=fileContent.Split('\r', '\n');
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+                var lineParts=line.Split(',');
+                var employee = new Employee
+                {
+                    Name = lineParts[0],
+                    Title = lineParts[1]
+
+                };
+                employees.Add(employee);
+            }
+
+            return employees;
+        }
+
+    }
+}
---
+++ Employeebook/FileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Employeebook
+{
+   static class FileHelper
+    {
+     public static async void WriteTextfileAsync(string filename,string content)
+        {
+            var localfolder = ApplicationData.Current.LocalFolder;
+            var textFile=await localfolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+            textStream.Dispose();
+
+        }
+        public static async Task<string> ReadTextFileAsync(string filename)
+        {
+            var localfolder = ApplicationData.Current.LocalFolder;
+            var textFile = await localfolder.GetFileAsync(filename);
+            var textStream = await textFile.OpenReadAsync();
+            var textReader = new DataReader(textStream);
+       
+            var textLength = textStream.Size;
+            await textReader.LoadAsync((uint)textLength);
+            return  textReader.ReadString((uint)textLength);
+
+
+        }
+    }
+}
